6a054275bd19f4ce4f18352350027f80dd3b719a
Steve Konves <stephen.konves@gmail.com>
2019-01-20 19:46:42 -0700

    feat: stub function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('This is the example typescript application!');
+import { DynamoDBStreamHandler } from 'aws-lambda';
+
+export const handler: DynamoDBStreamHandler = (event, context) => {
+  for (const record of event.Records) {
+    console.log('=========== RECORD ==========');
+    console.log(JSON.stringify(record.dynamodb.OldImage, null, 2));
+    console.log(JSON.stringify(record.dynamodb.NewImage, null, 2));
+  }
+};
